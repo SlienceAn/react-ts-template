99de4c458be42b473dd1d01769c8f9d56af4314f
silence <beast964089@gmail.com>
2023-04-26 17:51:57 +0800

    新增專案架構

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,12 @@
-"use strict";
-const a = "";
+import React from "react";
+import { render } from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const App = () => {
+    const func = () => {
+        console.log("../")
+    }
+    return <div className="hello">Hello</div>
+}
+
+render(<App />, rootElement)
